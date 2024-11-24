bd044806c328b312c9b06e17f25112fdcd4db85f
Krithik Manyam <95131617+MAGMA10MAN@users.noreply.github.com>
2024-11-24 22:28:17 +0530

    Updated movement + new feature
    Added a coordinate system for future update. Added trail when player speeds up beyond a threshold

--- My Pygame/Dont Slip.py
+++ My Pygame/Dont Slip.py
@@ -5,28 +5,8 @@ import math
 import time
 
 
-
-
-
-
-
-
-print("EPILEPSY WARNING! PROCEED AT YOUR OWN RISK")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 pygame.init()
+pygame.font.init()
 screen_width = 800
 screen_height = 600
 screen = pygame.display.set_mode((screen_width,screen_height))
@@ -40,10 +20,14 @@ x_dt = 0
 enemy_vel = 1
 friction = 0.01
 accel = 0.05
-max_dt = 5
+max_dt = 50
+level_x = 0
+level_y = 0
+my_font=pygame.font.SysFont("Comic Sans MS", 15)
+
 
 def handle_keys():
-    global x_dt, y_dt, player_pos,keys
+    global x_dt, y_dt, player_pos,keys, level_x, level_y
 
     if keys[pygame.K_w] or keys[pygame.K_UP]:
         if y_dt > -max_dt:
@@ -81,14 +65,30 @@ def handle_keys():
     player_pos.y += y_vel
     player_pos.x += x_vel
     
-    if player_pos.x >= screen_width+30:
+    """if player_pos.x >= screen_width+30:
         player_pos.x = 30
+        level_x+=1
     if player_pos.y >= screen_height+30:
         player_pos.y = 30
+        level_y-=1
     if player_pos.x <= -30:
         player_pos.x = screen_width - 30
+        level_x -= 1
     if player_pos.y <= -30:
         player_pos.y = screen_height - 30
+        level_y +=1"""
+    if player_pos.x >= screen_width+30:
+        player_pos.x = -29
+        level_x+=1
+    if player_pos.y >= screen_height+30:
+        player_pos.y = -29
+        level_y-=1
+    if player_pos.x <= -30:
+        player_pos.x = screen_width+29
+        level_x -= 1
+    if player_pos.y <= -30:
+        player_pos.y = screen_height+29
+        level_y +=1
 
     
 
@@ -98,6 +98,7 @@ def handle_keys():
 while running:
     y_vel = 3*y_dt
     x_vel = 3*x_dt
+    text_surface = my_font.render(f"{level_x}, {level_y}",False,(0,0,0))
     enemy_dist = math.sqrt(math.fabs(player_pos.y-enemy_pos.y)**2 + math.fabs(player_pos.x - enemy_pos.x)**2)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -107,8 +108,15 @@ while running:
     enemy_pos.x = (enemy_pos.x*(enemy_dist-enemy_vel) + player_pos.x*enemy_vel)/enemy_dist
     
     screen.fill("white")
+    screen.blit(text_surface, (0,0))
+    if (math.fabs(x_dt) + math.fabs(y_dt)) > 1:
+        i = int(math.fabs(x_dt) + math.fabs(y_dt))
+        for t in range(0,i):
+            pygame.draw.circle(screen, "cyan", (player_pos.x-t*0.5*x_vel,player_pos.y-t*0.5*y_vel), 35-35*t/i, width = 50)
     pygame.draw.circle(screen, "blue", player_pos, 35, width = 50)
-    
+
+        
+    """
     pygame.draw.circle(screen, "blue", (player_pos.x+screen_width,player_pos.y), 35, width = 50)
     pygame.draw.circle(screen, "blue", (player_pos.x-screen_width,player_pos.y), 35, width = 50)
     pygame.draw.circle(screen, "blue", (player_pos.x,player_pos.y+screen_height), 35, width = 50)
@@ -118,6 +126,7 @@ while running:
     pygame.draw.circle(screen, "blue", (player_pos.x-screen_width,player_pos.y-screen_height), 35, width = 50)
     pygame.draw.circle(screen, "blue", (player_pos.x-screen_width,player_pos.y+screen_height), 35, width = 50)
     pygame.draw.circle(screen, "blue", (player_pos.x+screen_width,player_pos.y-screen_height), 35, width = 50)
+    """
     keys = pygame.key.get_pressed()
 
 
@@ -132,6 +141,7 @@ while running:
     key_handler.start()
 
     pygame.draw.circle(screen,"red", enemy_pos,30)
+    pygame.ACTIVEEVENT
     pygame.display.flip()
 
     clock.tick(60)
